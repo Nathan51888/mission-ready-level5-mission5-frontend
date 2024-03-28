7917777020a081adaee8e54689c1e1c6a4fc36a9
Nathan51888 <nathanliang99@outlook.com>
2024-03-28 15:20:11 +1300

    added backend fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [messages, setMessages] = useState([]);
+    const [messagesMapped, setMessagesMapped] = useState([]);
+    const backendHost = import.meta.env.BACKEND_SERVICE_HOST || 'localhost';
+    const backendPort = import.meta.env.BACKEND_SERVICE_PORT || 3000;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function sendMessage() {
+        fetch(`http://${backendHost}:${backendPort}/send`)
+            .then(res => res.json())
+            .then(res => console.log(res));
+    }
+
+    function getMessages() {
+        fetch(`http://${backendHost}:${backendPort}/get`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setMessages(res);
+            });
+    }
+
+    useEffect(() => {
+        const mapped = messages.map((message, id) => {
+            console.log(message);
+            return <p key={id}>{message.message}</p>;
+        });
+        setMessagesMapped(mapped);
+    }, [messages]);
+
+    return (
+        <>
+            <button onClick={sendMessage}>Send Message</button>
+            <button onClick={getMessages}>Get Messages</button>
+            {messagesMapped}
+        </>
+    )
 }
 
 export default App
