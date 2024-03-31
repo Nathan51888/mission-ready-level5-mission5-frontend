5c8a6c215581c358a99fa26bca8dfdacf0d62c33
Nathan51888 <nathanliang99@outlook.com>
2024-03-31 17:38:46 +1300

    reset for week2

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,10 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-    const [messages, setMessages] = useState([]);
-    const [messagesMapped, setMessagesMapped] = useState([]);
-
-    function sendMessage() {
-        fetch('https://mission5.nathan.test/api/send')
-            .then(res => res.json())
-            .then(res => console.log(res));
-    }
-
-    function getMessages() {
-        fetch('https://mission5.nathan.test/api/get')
-            .then(res => res.json())
-            .then(res => {
-                console.log(res);
-                setMessages(res);
-            });
-    }
-
-    useEffect(() => {
-        const mapped = messages.map((message, id) => {
-            if (!message) return;
-            console.log(message);
-            return <p key={id}>{message.message}</p>;
-        });
-        setMessagesMapped(mapped);
-    }, [messages]);
 
     return (
         <>
-            <button onClick={sendMessage}>Send Message</button>
-            <button onClick={getMessages}>Get Messages</button>
-            {messagesMapped}
+            <div></div>
         </>
     )
 }
