1e9a9ee6618a3c7c4399898fa25353c25df665de
Nathan51888 <nathanliang99@outlook.com>
2024-04-01 17:40:22 +1300

    added station fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 // Components
@@ -8,10 +8,21 @@ import StationListComponent from './components/filter/StationListComponent';
 function App() {
     const [service, setService] = useState('');
     const [sortOption, setSortOption] = useState('');
+    const [stationList, setStationList] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/get')
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setStationList(res);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
         <>
-            <StationListComponent service={service} sortOption={sortOption} />
+            <StationListComponent stationList={stationList} service={service} sortOption={sortOption} />
         </>
     )
 }
--- src/components/filter/StationListComponent.jsx
+++ src/components/filter/StationListComponent.jsx
@@ -1,17 +1,7 @@
 import styles from './StationListComponent.module.css'
 import StationComponent from './StationComponent'
 
-function StationListComponent({ service, sortOption }) {
-    const stationList = [];
-    const station = {
-        name: "Z Royal Oak",
-        location: "700 mount Albert Road, Royal Oak, Auckland",
-        services: ['carWash', 'evCharging', 'preOrderCoffee', 'food', 'engineOils', 'lpgBottleSwap', 'giftCards']
-    };
-    for (let i = 0; i < 10; i++) {
-        stationList.push(station);
-    }
-
+function StationListComponent({ stationList, service, sortOption }) {
     const mappedList = stationList.map((item, index) => {
         return <StationComponent key={index} station={item} />
     });
